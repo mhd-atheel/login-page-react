b8c236fee62420b6f584bc6803a18df811012f12
mhd-atheel <aathilmazz1234@gmail.com>
2022-09-19 20:28:13 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='container'>
+        <h1>Welcome Back</h1>
+        <form>
+        <label>Email</label>
+        <input type="text" />
+        <label>Password</label>
+        <input type="password" />
+        <input type="submit" className="button" value="Sign In" onClick={()=>{alert("Login SuccessFully")}} />
+        </form>
+        
+      </div>
     </div>
   );
 }
